98d768db93299621eb4ba9fd35b404d737d9d1fe
phipunkt <122401641+phipunkt@users.noreply.github.com>
2024-04-25 10:59:52 +0200

    Revert dummy ip

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 TIMEZONE = 1 # Number of hours difference between UTC (positive or negative number)
 DST = True # If True, enable automatic DST time setting. False: No DST correction used.
-IP_PV = "10.0.42.53" # IP or hostname pv inverter Fronius, change to your your configuration
-IP_WB = "10.0.42.60" # IP or hostname go-e charger, change to your configuration
+IP_PV = "xxx.xxx.xxx.xxx" # IP or hostname pv inverter Fronius, change to your your configuration
+IP_WB = "xxx.xxx.xxx.xxx" # IP or hostname go-e charger, change to your configuration
 I_MIN = 6 # Minimum charging current for car in ampere (usually 6 A)
 I_MAX = 16 # Maximum charging current allowed, electric installation and charger type. 16 A is for 11 kW charger.
 I_MAX_1P = 16 # Maximum current limit for 1 phase, set to country limit if less than I_MAX
